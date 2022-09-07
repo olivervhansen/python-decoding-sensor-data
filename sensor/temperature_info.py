ceafd845dda9494e958e24421826bc3a67784c0e
Oliver Hansen <ohansen@kingcounty.gov>
2022-09-07 15:57:59 -0700

    change __ to single _ per tests

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -5,7 +5,7 @@ from house_info import HouseInfo
 
 
 class TemperatureData(HouseInfo):
-    def __convert_data(self, data):
+    def _convert_data(self, data):
         recs = []
 
         for rec in data:
@@ -14,8 +14,8 @@ class TemperatureData(HouseInfo):
 
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("temperature", rec_area)
-        return self.__convert_data(recs)
+        return self._convert_data(recs)
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("temperature", rec_date)
-        return self.__convert_data(recs)
+        return self._convert_data(recs)
